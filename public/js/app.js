a92142853e25982d764efdeaeec962f23f17f599
gotylergo <tk03759@gmail.com>
2018-05-05 13:54:00 -0700

    Append page from destinations object and remove placeholders

--- public/js/app.js
+++ public/js/app.js
@@ -3,17 +3,22 @@ let MOCK_DESTINATIONS = {
         {
             "id": "111111",
             "name": "Hawaii",
-            "completed": "no"
+            "completed": "no",
+            "activities": ["Go surfing","Meet Lilo & Stitch","Fly over an active volcano"]
         },
         {
             "id": "222222",
             "name": "New York City",
-            "completed": "no"
+            "completed": "no",
+            "activities": ["See Times Square","Climb the Statue of Liberty","Walk through Central Park"]
+
         },
         {
             "id": "333333",
             "name": "Tokyo",
-            "completed": "no"
+            "completed": "no",
+            "activities": ["Photo with Mickey at Tokyo Disneyland","Shop in the Ginza District","Meet a Geisha at Omotenashi Nihonbashi"]
+
         }
     ]
 };
@@ -24,10 +29,14 @@ function getMyDestinations(callbackFn) {
 
 // why don't these have semicolons but the one above does
 
-function displaMyDestinations(data) {
+function displayMyDestinations(data) {
     for (i in data.destinations) {
-        $('body').append('<div>' + data.destinations[i].name + '</div');
-        // try this with a template string
+        $('.container').append(`<div class="card dest-card card-shadow hawaii-card i${data.destinations[i].id}"><div class="close-card-button hide-me"><a href="#"><i class="fa fa-times-circle"></i></a></div><h3 contenteditable="true">${data.destinations[i].name}</h3><ul class="activitiesList"></ul></div>`);
+        $(data.destinations[i].activities).each(function(activity) {
+            let thisDestination = `.i${data.destinations[i].id}`;
+            $(thisDestination).append(`<li>${data.destinations[i].activities[activity]}</li>`);
+        });
+
     }
 }
 
@@ -38,3 +47,17 @@ function getAndDisplayMyDestinations() {
 $(function () {
     getAndDisplayMyDestinations();
 })
+
+$(".card").on("click", function(e) {
+    e.preventDefault();
+    $(this).addClass("card-open");
+    $(".close-card-button").removeClass("hide-me");
+});
+
+$(".close-card-button").on("click", function(e) {
+    e.preventDefault();
+    console.log(this.parentElement);
+    $(this.parentElement).removeClass("card-open");
+    console.log(this);
+    $(this).addClass("hide-me");
+});
