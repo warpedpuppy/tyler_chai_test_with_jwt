6053de439a2ba5c75a9581760b4830b0ffb5bcc7
gotylergo <tk03759@gmail.com>
2018-05-05 17:46:03 -0700

    Add complete button and header.

--- public/js/app.js
+++ public/js/app.js
@@ -4,20 +4,20 @@ let MOCK_DESTINATIONS = {
             "id": "111111",
             "name": "Hawaii",
             "completed": "no",
-            "activities": ["Go surfing","Meet Lilo & Stitch","Fly over an active volcano"]
+            "activities": ["Go surfing", "Meet Lilo & Stitch", "Fly over an active volcano"]
         },
         {
             "id": "222222",
             "name": "New York City",
             "completed": "no",
-            "activities": ["See Times Square","Climb the Statue of Liberty","Walk through Central Park"]
+            "activities": ["See Times Square", "Climb the Statue of Liberty", "Walk through Central Park"]
 
         },
         {
             "id": "333333",
             "name": "Tokyo",
             "completed": "no",
-            "activities": ["Photo with Mickey at Tokyo Disneyland","Shop in the Ginza District","Meet a Geisha at Omotenashi Nihonbashi"]
+            "activities": ["Photo with Mickey at Tokyo Disneyland", "Shop in the Ginza District", "Meet a Geisha at Omotenashi Nihonbashi"]
 
         }
     ]
@@ -32,7 +32,7 @@ function getMyDestinations(callbackFn) {
 function displayMyDestinations(data) {
     for (i in data.destinations) {
         $('.container').append(`<div class="card dest-card card-shadow hawaii-card i${data.destinations[i].id}"><div class="close-card-button hide-me"><a href="#"><i class="fa fa-times-circle"></i></a></div><h3 contenteditable="true">${data.destinations[i].name}</h3><ul class="activitiesList"></ul></div>`);
-        $(data.destinations[i].activities).each(function(activity) {
+        $(data.destinations[i].activities).each(function (activity) {
             let thisDestination = `.i${data.destinations[i].id}`;
             $(thisDestination).append(`<li>${data.destinations[i].activities[activity]}</li>`);
         });
@@ -48,16 +48,15 @@ $(function () {
     getAndDisplayMyDestinations();
 })
 
-$(".card").on("click", function(e) {
+$(".card").on("click", function (e) {
     e.preventDefault();
-    $(this).addClass("card-open");
-    $(".close-card-button").removeClass("hide-me");
+    $(this).addClass("card-open").removeClass("card");
+    $(".close-card-button, .complete-card-button").removeClass("hide-me")
+    $(".card, .h1").addClass("hide-me");
 });
 
-$(".close-card-button").on("click", function(e) {
+$(".close-card-button").on("click", function (e) {
     e.preventDefault();
-    console.log(this.parentElement);
-    $(this.parentElement).removeClass("card-open");
-    console.log(this);
-    $(this).addClass("hide-me");
+    $(".close-card-button, .complete-card-button").addClass("hide-me");
+    $(".card-open").removeClass("card-open");
 });
