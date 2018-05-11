e93e3a33f2ce78c55c2a263f7c60971fba0d9a8c
gotylergo <tk03759@gmail.com>
2018-05-10 21:41:13 -0700

    Code login tab menu

--- public/js/index.js
+++ public/js/index.js
@@ -12,9 +12,15 @@ $("body").on("click", ".register-tab", function(e) {
     e.preventDefault();
     $("#login-form").addClass("hide-me");
     $("#register-form").removeClass("hide-me");
+    $(".register-tab").addClass("login-menu-active").removeClass("login-menu-passive");
+    $(".login-tab").removeClass("login-menu-active").addClass("login-menu-passive");
 });
 $("body").on("click", ".login-tab", function(e) {
     e.preventDefault();
     $("#register-form").addClass("hide-me");
     $("#login-form").removeClass("hide-me");
+    $(".login-tab").addClass("login-menu-active").removeClass("login-menu-passive");
+    $(".register-tab").removeClass("login-menu-active").addClass("login-menu-passive");
+
+
 });
