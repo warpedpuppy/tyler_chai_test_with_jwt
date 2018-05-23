8e4515edfbef2477e2297a37d8063147b3107b7b
gotylergo <tk03759@gmail.com>
2018-05-22 20:31:57 -0700

    Format code

--- users/models.js
+++ users/models.js
@@ -14,24 +14,29 @@ const UserSchema = mongoose.Schema({
     type: String,
     required: true
   },
-  name: {type: String, default: ''},
+  name: {
+    type: String,
+    default: ''
+  }
 });
 
-UserSchema.methods.serialize = function() {
+UserSchema.methods.serialize = function () {
   return {
     username: this.username || '',
     name: this.name || ''
   };
 };
 
-UserSchema.methods.validatePassword = function(password) {
+UserSchema.methods.validatePassword = function (password) {
   return bcrypt.compare(password, this.password);
 };
 
-UserSchema.statics.hashPassword = function(password) {
+UserSchema.statics.hashPassword = function (password) {
   return bcrypt.hash(password, 10);
 };
 
 const User = mongoose.model('User', UserSchema);
 
-module.exports = {User};
+console.log(`This is users/models ${typeof User} - ${User}`)
+
+module.exports = { User };
